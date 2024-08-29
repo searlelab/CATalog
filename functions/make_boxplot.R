f739b0e3e3479e5eca2ca5c77865ad71ebadbdbe
Alex Joyce <alexwilliamjoyce@gmail.com>
2024-08-29 19:27:19 -0400

    Made some adjustments to the plots

--- functions/make_boxplot.R
+++ functions/make_boxplot.R
@@ -1,10 +1,15 @@
 make_boxplot <- function(df, name){
 	plot <- ggplot(df, aes(x = as.factor(group), y = values, fill = group, label = labels))+
 		geom_boxplot()+
-		geom_jitter(color = "blue", position = position_jitter(seed = 1))+
-		geom_text(position = position_jitter(seed = 1))+
-		xlab("sample")+
+		theme(axis.title.x=element_blank(),
+		      axis.text.x=element_blank(),
+		      axis.ticks.x=element_blank())+
+		scale_fill_manual(values = c('#D55382', '#003F5C', '#FFA600'))+
+		#geom_jitter(color = "cyan4", position = position_jitter(seed = 1))+
+		geom_text(color = "cyan4", position = position_jitter(seed = 1))+
+		ylab("relative intensity")+
 		ggtitle(name)
 	plot
 }
+
 
