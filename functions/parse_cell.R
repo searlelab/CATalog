0c91d79a63b73255e8c7aabc87bc1cdcafe11374
Alex Joyce <alexwilliamjoyce@gmail.com>
2024-09-16 12:48:07 -0400

    Fixed a bug involving no matching GO terms

--- functions/parse_cell.R
+++ functions/parse_cell.R
@@ -1,10 +1,16 @@
 parse_cell <- function(protein, index, data){
 	r <- data%>%
-	filter(Column2 == protein)
-	info <- r[,index] 
-	semi_parsed <- unlist(strsplit(info, split = ';'))
-	df <- data.frame(semi_parsed)%>% 
-		tidyr::separate(semi_parsed, into = c("Description", "GO_ID"), sep = "\\[|\\]", extra = "drop", fill = "right")
+		filter(Column2 == protein)
+	if(nrow(r) == 0){
+		df <- NULL
+		print("no GO terms found")
+	}
+	else{
+		info <- r[,index] 
+		semi_parsed <- unlist(strsplit(info, split = ';'))
+		df <- data.frame(semi_parsed)%>% 
+			tidyr::separate(semi_parsed, into = c("Description", "GO_ID"), sep = "\\[|\\]", extra = "drop", fill = "right")
+	}
 	df
 }
 
