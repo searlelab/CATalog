02e839c67c267cee6a67f3fd8d8df7cad4145923
Alex Joyce <alexwilliamjoyce@gmail.com>
2024-10-18 11:12:01 -0400

    Removed the v1.0 version of the app and renamed the 1.1 version to 'app.R'. The entire 1.0 codebase hass been archived.

--- app.R
+++ app.R
@@ -4,193 +4,181 @@ library(ggplot2)
 library(shinydashboard)
 library(tidyverse)
 
-source('./functions/boxplot_wrapper.R')
-source('./functions/cell_parser_wrapper.R')
-source('./functions/fetch_go_info.R')
-source('./functions/filter_foreground.R')
-source('./functions/format_data.R')
-source('./functions/go_chunk.R')
-source('./functions/go_column_mapper.R')
-source('./functions/go_protein_mapper.R')
-source('./functions/load_background_data.R')
-source('./functions/load_foreground_data.R')
-source('./functions/make_boxplot_annotated.R')
-source('./functions/make_boxplot_unannotated.R')
-source('./functions/parse_cell.R')
-source('./functions/search_go_data_background.R')
-source('./functions/search_go_data_foreground.R')
-source('./functions/spoof_dataframe.R')
-source('./functions/update_background.R')
-
-GO_data <- read.csv("./data/go_data.csv")
+#static datasets
+go_data <- read.csv("./data/go_data.csv")
 deltas <- read.csv("./data/deltas.csv")
+background <- read.csv("./data/background.csv")
+demographics <- read.csv("./data/demographics.csv")
+
+
+source('./functions/load_foreground_data.R')
+source('./functions/map_entry_to_index.R')
+
+#boxplot functions
+source('./functions/boxplot/boxplot_driver.R')
+source('./functions/boxplot/format_data.R')
+source('./functions/boxplot/make_boxplot_annotated.R')
+source('./functions/boxplot/make_boxplot_unannotated.R')
+
+#gene ontology functions
+source('./functions/gene_ontology/go_processor.R')
+source('./functions/gene_ontology/parse_cell.R')
+source('./functions/gene_ontology/set_ontology.R')
+source('./functions/gene_ontology/spoof_dataframe.R')
+
+#search-reated functions
+source('./functions/search/search_for_go_keyword.R')
+source('./functions/search/go_column_mapper.R')
+
+#filtering functions
+source('./functions/filters/filter_by_highest_biofluid.R')
+
 
 ui <- dashboardPage(skin = "black",
-    dashboardHeader(title = tags$img(src='https://i.ibb.co/x6tH34j/logo4.png', height = '60', width = '120')),
-    dashboardSidebar(
-        textInput("keyword", "Filter proteins by GO: ", value = ""),
-        actionButton("searchButton", "Search"),
-        actionButton("resetButton", "reset"),
-        selectInput("sampleType", "Filter by highest biofluid:",
-                    choice = c("all", "urine", "serum", "plasma")),
-        actionButton("filterButton", "Filter"),
-        radioButtons("plot_labels", "Sample annotation: ",
-                     c("off", "on")),
-        radioButtons("go_item", "GO Data: ",
-                     c("biological process",
-                       "cellular compartment",
-                       "molecular function"),
-                     selected = "biological process")
-    ),
-      dashboardBody(
-        fluidRow(
-          column(width = 8,
-            box(width = NULL, DT::dataTableOutput("display"), 
-              style = "height:400px; overflow-y: scroll; overflow-x: scroll;"),
-            box(width = NULL, DT::dataTableOutput("results"),
-                style = "height: 200px; overflow-y: scroll; overflow-x: scroll;")
-            ),
-          column(width = 4,
-            box(width = NULL, plotOutput("boxplot", height = 300, width = 250)),
-            box(width = NULL, div(tableOutput("demo"), style = "font-size:70%; overflow-y: scroll"),
-              style = "height: 100px")
-          )
-        )
-    )
+                    dashboardHeader(title = tags$img(src='https://i.ibb.co/x6tH34j/logo4.png', height = '60', width = '120')),
+                    dashboardSidebar(
+                      textInput("keyword", "Filter proteins by GO: ", value = ""),
+                      actionButton("searchButton", "Search"),
+                      actionButton("resetButton", "reset"),
+                      selectInput("sampleType", "Filter by highest biofluid:",
+                                  choice = c("all", "urine", "serum", "plasma")),
+                      actionButton("filterButton", "Filter"),
+                      radioButtons("plot_labels", "Sample annotation: ",
+                                   c("off", "on")),
+                      radioButtons("go_item", "GO Data: ",
+                                   c("biological process",
+                                     "cellular compartment",
+                                     "molecular function"),
+                                   selected = "biological process")
+                    ),
+                    dashboardBody(
+                      fluidRow(
+                        column(width = 8,
+                               box(width = NULL, DT::dataTableOutput("display"), 
+                                   style = "height:400px; overflow-y: scroll; overflow-x: scroll;"),
+                               box(width = NULL, DT::dataTableOutput("results"),
+                                   style = "height: 200px; overflow-y: scroll; overflow-x: scroll;")
+                        ),
+                        column(width = 4,
+                               box(width = NULL, plotOutput("boxplot", height = 300, width = 250)),
+                               box(width = NULL, div(tableOutput("demo"), style = "font-size:70%; overflow-y: scroll"),
+                                   style = "height: 100px")
+                        )
+                      )
+                    )
 )
 
-
 server <- function(input, output, session){
+  Database <- reactiveValues()
+  Search <- reactiveValues() #search-related features
+  Params <- reactiveValues() #user settings
+  Global <- reactiveValues() #elements that are not related to the core database but need to be accessed by disprate functions
   
-  demographics <- read.csv("./data/demographics.csv")
+  Database$go_list = NULL
   
-  main <- reactiveValues()
-  search <- reactiveValues()
-
-  main$go_list = NULL
-  
+  #loading the foreground data
   foreground <- reactive({
     load_foreground_data()
   })
   
   observeEvent(foreground(),{
-    main$data <- foreground()
-  })
-
-  background <- reactive({
-    load_background_data()
+    Database$dataset <- foreground()
   })
   
-  observeEvent(background(),{
-    main$back_data <- background()
-  })
+  #setting up the search parameters
+  Search$cache <- NULL
+  Search$is_ongoing <- FALSE
   
-  main$search_cache_foreground <- NULL
-  main$search_cache_background <- NULL
-  search$ongoing <- FALSE
-
-  observeEvent(input$display_rows_selected,{
-    main$row_selected <- input$display_rows_selected
-    go_list <- go_chunk(data = main$data,
-                        row_id = input$display_rows_selected,
-                        GO_data
-    )
-    main$go_list <- go_list
-    main$go_element <- fetch_go_info(go_list, field = input$go_item)
-  })
-  
+  #connected to the GO field buttons
+  #the seemingly redundant code here is used to switch between GO categories for a given protein when clicking other buttons
   observeEvent(input$go_item,{
-    main$go_element <- fetch_go_info(go_list = main$go_list, field = input$go_item)
+    Params$go_field <- input$go_item
+    Database$ontology <- set_ontology(go_list = Global$all_ontologies, field = Params$go_field)
   })
   
+  #triggers when clicking on a row of the main display
+  observeEvent(input$display_rows_selected,{
+    Database$current_entry <- map_entry_to_index(Database$dataset, index = input$display_rows_selected)
+    Global$all_ontologies <- go_processor(entry = Database$current_entry, go_data)
+    Database$ontology <- set_ontology(go_list = Global$all_ontologies, field = Params$go_field)
+    print(head(Database$ontology))
+  })
+  
+  output$display <- DT::renderDataTable({
+    datatable(Database$dataset, selection = 'single')
+  })
+  
+  #GO result display
+  output$results <- DT::renderDataTable({
+    req(input$display_rows_selected)
+    Database$ontology
+  })
+  
+  #triggers when pushing the search button
   observeEvent(input$searchButton,{
-    main$data <- foreground()
-    main$back_data <- background()
-    index <- go_column_mapper(input$go_item)
-    res <- search_go_data_foreground(GO_data, main$data, index, word = input$keyword)
-    main$data <- res
-    res_background <- search_go_data_background(GO_data, main$back_data, index, word = input$keyword)
-    main$back_data <- res_background
-    main$search_cache_foreground <- res
-    main$search_cache_background <- res_background
-    print(nrow(main$search_cache))
-    search$ongoing <- TRUE
-    print(search$ongoing)
+    Database$dataset <- foreground() #reset the foreground data
+    index <- go_column_mapper(input$go_item) #need to figure out how to circumvent this function
+    search_results <- search_for_go_keyword(go_data, Database$dataset, go_field = index, keyword = input$keyword)
+    Database$dataset <- search_results #set the search results
+    Search$cache <- search_results #set the cache for the filtering process
+    Search$is_ongoing <- TRUE #state that we are currently searching
   })
   
+  #the search button is fundementally linked to the reset button
   observeEvent(input$resetButton,{
-    main$data <- foreground()
-    main$back_data <- background()
+    Database$dataset <- foreground()
     updateTextInput(session,"keyword", value="")
-    search$ongoing <- FALSE
-    #print(search$ongoing)
+    Search$is_ongoing <- FALSE
   })
   
+  #code for the sample selector; determines what to filter by
   observeEvent(input$sampleType,{
-    main$sample_selection <- input$sampleType
-    #print(main$sample_selection)
+    Params$target_sample <- input$sampleType
   })
   
-
+  
+  #code for the filtering process
   observeEvent(input$filterButton,{
-
+    if(Params$target_sample == "all" & Search$is_ongoing == FALSE){ 
+      Database$dataset <- foreground() #reset to the entire dataset
+    }
+    else if(Params$target_sample == "all" & Search$is_ongoing == TRUE){
+      Database$dataset <- Search$cache #reset to the cached search data
+    }
     
-    if(main$sample_selection == "all" & search$ongoing == FALSE){ 
-      main$data <- foreground() 
-      main$back_data <- background()
-    }
-    else if(main$sample_selection == "all" & search$ongoing == TRUE){
-      main$data <- main$search_cache_foreground
-      main$back_data <- main$search_cache_background
-    }
- 
-    else if(main$sample_selection != "all" & search$ongoing == TRUE){
-      output <- filter_foreground(main$search_cache_foreground, deltas, field = main$sample_selection)
-      background_output <- update_background(output, main$search_cache_background)
-      main$data <- output
-      main$back_data <- background_output
+    else if(Params$target_sample != "all" & Search$is_ongoing == TRUE){
+      Database$dataset <- filter_by_highest_biofluid(Search$cache, deltas, field = Params$target_sample)
     }
     else{
-      main$data <- foreground()
-      main$back_data <- background()
-      output <- filter_foreground(main$data, deltas, field = main$sample_selection)
-      background_output <- update_background(output, main$back_data)
-      main$data <- output
-      main$back_data <- background_output
+      Database$dataset <- foreground()
+      Database$dataset <- filter_by_highest_biofluid(Database$dataset, deltas, field = Params$target_sample)
     }
   })
   
   #observer for the plot annotation
   observeEvent(input$plot_labels,{
-    main$plot_annotation <- input$plot_labels
-    if(main$plot_annotation == "off"){
-      main$demographics <- NULL
+    Params$is_annotated <- input$plot_labels
+    if(Params$is_annotated == "off"){
+      Params$demographics <- NULL
     }
-    if(main$plot_annotation == "on"){
-      main$demographics <- demographics
+    if(Params$is_annotated == "on"){
+      Params$demographics <- demographics
     }
-  })
-  
-  
-  output$results <- DT::renderDataTable({
-    req(input$display_rows_selected)
-    main$go_element
   })
   
   output$demo <- renderTable({
-    main$demographics
+    Params$demographics
   })
   
-  output$display <- DT::renderDataTable({
-    datatable(main$data, selection = 'single')
-  })
-  
+  #output for the plot
   output$boxplot <- renderPlot({
     req(input$display_rows_selected)
-    s = input$display_rows_selected
-    plot <- boxplot_wrapper(data = main$back_data, i = s, flag = main$plot_annotation)
+    plot <- boxplot_driver(data = background, entry = Database$current_entry, flag = Params$is_annotated)
     plot
   })
+  
+  
+ 
   
 }
 
