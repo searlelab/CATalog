afd92fb43ea72825b3860d32b68db9f34b122875
Alex Joyce <alexwilliamjoyce@gmail.com>
2024-12-04 10:59:03 -0500

    Fixed a typo that caused the app to crash

--- functions/filters/apply_demographic_filter.R
+++ functions/filters/apply_demographic_filter.R
@@ -1,9 +1,11 @@
 apply_demographic_filter <- function(background, demographics, max_age, max_bsc){
 	if(max_age < 11 ){
 		background <- filter_background(background, demographics, target = "Age", max_value = max_age)
+		print("Age filter passed")
 	}
 	if(max_bsc < 10){
-		background <- filter_background(background, demograpics, target = "BCS", max_value = max_bsc)
+		background <- filter_background(background, demographics, target = "BSC", max_value = max_bsc)
+		print("BSC filter passed")
 	}
 	background
 }
--- functions/filters/filter_background.R
+++ functions/filters/filter_background.R
@@ -1,17 +1,24 @@
 filter_background <- function(data, demographics, target, max_value){
+	print(target)
 	r <- demographics%>%
 		filter(Cat == target)
-
+	print(head(r))
+	print("check 1")
 	r <- data.frame(t(r[-1]))
+	print(head(r))
+	print("check 1.1")
 	r[,1] <- as.numeric(r[,1])
+	print("check 1.2")
 	n <- row.names(r %>% filter(r[,1] <= max_value))
+	print("check 1.3")
 	metadata <- data%>%select(1:3)
+	print("check 1.4")
 	data <- data[,-1:-3]
-
+	print("check 2")
 	pattern <- paste(n, collapse = "|")
 	filtered_data <- data[,grepl(pattern, names(data))]
 	data <- cbind(metadata, filtered_data)
-
+	print("sucessfully filtered background")
 	data
 }
 
