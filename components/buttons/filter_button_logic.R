97df08122124051b451336d563e6a471ab5d4fdf
Alex Joyce <alexwilliamjoyce@gmail.com>
2025-01-30 09:48:50 -0500

    Removed some debug code; added an alert to tell the user to select a row before filtering.

--- components/buttons/filter_button_logic.R
+++ components/buttons/filter_button_logic.R
@@ -1,10 +1,12 @@
 filter_button_logic <- function(input, Database, Search, PlotManager, demographics, output, session){
 	observeEvent(input$filter_button,{
+		if(is.null(input$main_display_rows_selected)){
+			shinyalert(print("Please select a protein"))
+		}
 		req(input$main_display_rows_selected)
     		Database$background <- Database$background_cache
 		previously_selected <- input$main_display_rows_selected
 		if(Search$ongoing == TRUE){
-			print("using the cached search results")
 			Database$background <- filter_background_by_cache(Database$background, Search$cache)
 		}
 
@@ -12,22 +14,15 @@ filter_button_logic <- function(input, Database, Search, PlotManager, demographi
       			Database$background <- filter_background_by_demographics(Database$background, demographics, demographic_field = "Age", value = input$age_filter)
 		}
 		if(input$bsc_filter > 0 && input$bsc_filter < 8){
-			print("filtering by BSC")
-			print(paste("the value is: ", input$bsc_filter))
 			Database$background <- filter_background_by_demographics(Database$background, demographics, demographic_field = "BSC", value = input$bsc_filter)
-			print("applied bsc filter")
 		}
 		if(input$sex_filter != "Both"){
-			print("filtering by sex")
-			print(paste("the value is: ", input$sex_filter))
 			Database$background <- filter_background_by_demographics(Database$background, demographics, demographic_field = "Sex", value = input$sex_filter)
-			print("applied sex filter")
 		}
 
       		Database$foreground <- generate_foreground(Database$background)
 		if(input$biofluid_type != "All" && input$delta_threshold > 0){
       			Database$foreground <- filter_foreground_by_highest_biofluid(Database$foreground, biofluid = input$biofluid_type, delta_threshold = input$delta_threshold)
-			print("applied the foreground filter")
 		}
 		
 		Database$demographics <- update_demographics(Database$background, demographics)
@@ -45,8 +40,8 @@ filter_button_logic <- function(input, Database, Search, PlotManager, demographi
 			PlotManager$current_plot <- PlotManager$scatterplot
 		}
 
-		proxy <- DT::dataTableProxy("main_display") # Replace "main_display" with your actual DataTable ID
-    		DT::selectRows(proxy, previously_selected)
+		proxy <- DT::dataTableProxy("main_display") 
+		DT::selectRows(proxy, previously_selected)
 
 
 
